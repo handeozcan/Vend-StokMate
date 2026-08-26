c66897c66e15d6d4ee1399eec4cec211c420646e
handeozcan <handeeozcann@gmail.com>
2026-08-26 19:03:00 +0300

    chore: drop unused formstate binding

--- stokmate-mobile/src/app/(auth)/login.tsx
+++ stokmate-mobile/src/app/(auth)/login.tsx
@@ -15,7 +15,6 @@ export default function Login() {
   const {
     control,
     handleSubmit,
-    formState: { errors },
   } = useForm<LoginFormValues>({
     resolver: zodResolver(loginSchema),
     defaultValues: { email: '', password: '' },
